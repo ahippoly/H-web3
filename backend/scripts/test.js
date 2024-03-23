762428b85bbc5d4b430a62ee726f92ee5d5da0d9
anii76 <ka_bourouina@esi.dz>
2024-03-23 22:54:24 +0100

    feat: add pinata code

--- backend/scripts/test.js
+++ backend/scripts/test.js
@@ -6,7 +6,7 @@ async function testDeployement() {
         "0xEB57863C01aF79814b6B20c416c13520ee51119f"
     );
 
-    await contract.addItem("0x9473EC0057AcBBa6b6E1d6af50d14C6343C0817A","");
+    await contract.safeMint("0x9473EC0057AcBBa6b6E1d6af50d14C6343C0817A","https://ipfs.io/ipfs/QmVxeTWLdQRkHbL9pyR99S7FPDZ6fZU3faATrFg18famgF1.jpg");
 }
 
 testDeployement().catch((error) => {
